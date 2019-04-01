ab9fa2aa4019f5768112202757dc6243c64648fc
ahuerta <hutarmando10911@gmail.com>
2019-04-01 16:41:46 -0600

    Creación de git ignore, creacion del menu

--- view/adminView.php
+++ view/adminView.php
@@ -64,7 +64,7 @@
                 <div class='col-md-12'>
                     <div class="jumbotron">
                         <h1 class="display-4">Bienvenido <?php echo $_SESSION['usuario']; ?> a TPM</h1>
-                        <p class="lead">Esta plataforma tiene como objetivo registrar los lup's para Cuernavaca y Toluca.</p>
+                        <p class="lead">Esta plataforma tiene como objetivo registrar las lup's para Cuernavaca y Toluca.</p>
                         <hr class="my-4">
                         <p>Para más información comunicate con los responsables directos de la plataforma que se encuentran en el pie de pagina!</p>
                     </div>
---
+++ view/menuUsuario.php
@@ -0,0 +1,32 @@
+<!-- Nav Usuario Clave
+Acceso solo usuarios que registran las lups!
+-->
+<nav class="navbar navbar-expand-lg navbar-light bg-light navbar-dark bg-dark rounded">
+    <a class="navbar-brand" href="">Usuario Clave TPM</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="index.php?controller=login&action=usuarioActivo">Inicio<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Seguimiento de Lup's
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="index.php?controller=lups&action=index">Creacion de Lup</a>
+                    <a class="dropdown-item" href="index.php?controller=lups&action=leerLup">Revisar Lup</a>                   
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="index.php?controller=login&action=salir">Salir</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+<!--.\ Nav Bar -->
+
+
+
