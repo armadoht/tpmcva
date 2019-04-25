469d11386f0a7c212522af2ff01dc68890d0b95d
ahuerta <hutarmando10911@gmail.com>
2019-04-25 09:35:30 -0500

    Cordinadora de TPM

--- view/lupsView.php
+++ view/lupsView.php
@@ -298,31 +298,7 @@
         </div>
         <!-- .\container-->
         <div class="bor"></div>
-        <footer>
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-6">
-                        <div class="widget">
-                            <h4>Responsables de la plataforma</h4>
-                            <p></p>
-                            <ul>
-                                <li><i class="fa fa-angle-right"></i><a href="#">Martin Espinoza - Gerente de Mantenimiento Corrugado</a></li>
-                                <li><i class="fa fa-angle-right"></i><a href="#">Juan Jose Flores - Coordinador de TPM</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <!-- .\ row -->
-                <div class="row">
-                    <hr/>
-                    <div class="col-md-12">
-                        <div class="copy pull-left">
-                            <p> Copyright &copy; <a href="#">www.grupak.com.mx</a> | Designed by <a href="#"></a>Ing. Armando Huerta Tolentino</a></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </footer>
+        <?php include("footer.php") ?>
         <!-- .\ End -->
         <!-- jQuery -->
         <script type="text/javascript" src="view/js/query/jquery-min.js"></script>
